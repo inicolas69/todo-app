50e42504408a15228a12ff57de60dd6e307c68fb
Nicolas Iniesta <inicolas69@gmail.com>
2020-09-30 23:07:54 +0200

    Todo app avancée

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 @click="test">Hello !</h1>
+  <form @submit.prevent="addNewTodo">
+    <label>Todo</label>
+    <input v-model="newTodo" name="newTodo">
+    <button>Add new todo !</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      <h3 :class="{ done: todo.done }" @click="toggleDone(todo)"> {{todo.content}} </h3>
+    </li>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref } from 'vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const newTodo = ref('');
+    const todos = ref([]);
+
+    function test() {
+      console.log('Toto');
+    }
+
+    function addNewTodo() {
+      todos.value.push({
+        id: Date.now(),
+        done: false,
+        content: newTodo.value,
+      });
+      console.log(todos.value);
+      console.log(newTodo.value);
+    }
+
+    function toggleDone(todo) {
+      todo.done = !todo.done
+    }
+
+    return {
+      test,
+      addNewTodo,
+      newTodo,
+      todos,
+      toggleDone
+    };
+  },
+};
 </script>
 
 <style>
@@ -22,5 +56,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.done {
+  text-decoration: line-through;
 }
 </style>
